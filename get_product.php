e6d231ae6e3acab944ab433d8ca16d03b50003f3
Muhammad Fiqri <muhfikri6565@gmail.com>
2023-08-12 16:42:02 +0700

    get product json fix

--- get_product.php
+++ get_product.php
@@ -14,22 +14,18 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    echo "Connected successfully";
+    echo "Connected successfully,";
 
     $sql = "SELECT product_id, product_name, product_price, product_category, product_img FROM product";
     $result = $conn->query($sql);
 
+    $product_data = array();
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {
-            $matrix_product_data = array();
-            array_push($matrix_product_data,array("product_id"=>$row["product_id"],
-            "product_name"=>$row["product_name"],
-            "product_price"=>$row["product_price"],
-            "product_category"=>$row["product_category"],
-            "product_img"=>$row["product_img"]));
-            echo json_encode($matrix_product_data);
+            array_push($product_data,$row);
         }
+        echo json_encode($product_data);
       } else {
         echo "0 results";
       }
